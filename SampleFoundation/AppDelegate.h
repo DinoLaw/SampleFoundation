8fbb02ac0dabb6290ba9a0d2696b30d9790c6afe
Dino Law <dinolaw@nmg.com.hk>
2014-07-23 18:00:04 +0800

    first commit

---
+++ SampleFoundation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SampleFoundation
+//
+//  Created by diNo Law on 2014/7/23.
+//  Copyright (c) 2014年 NMG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SampleFoundation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SampleFoundation
+//
+//  Created by diNo Law on 2014/7/23.
+//  Copyright (c) 2014年 NMG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
